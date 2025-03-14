9ca68424e183785053676505ed3f93de257778d6
Danil Chekantsev <chekantsev.danil2@gmail.com>
2025-03-14 08:23:13 +0300

    Initial Commit

---
+++ MoneyExchageSUI/MoneyExchageSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyExchageSUIApp.swift
+//  MoneyExchageSUI
+//
+//  Created by Danil Chekantsev on 14.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct MoneyExchageSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
